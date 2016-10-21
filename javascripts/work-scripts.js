a606747596ffd60b7a3ad8182afa04c3c0085468
Ledovit <drd8@mail.ru>
2016-10-21 15:58:20 +0300

    подсказка убирается

--- javascripts/work-scripts.js
+++ javascripts/work-scripts.js
@@ -41,31 +41,11 @@ $( document ).ready(function() {
             var block =$(this).closest('.hint-container').find('.hint-container__text');
             block.addClass("on");
           return false;
-        $(function(){
-            $(document).click(function(event) {
-    //          $(".hint-container__text").hide("fast");
-                $(".hint-container__text").removeClass("on");
-              event.stopPropagation();
-            });
-          });
       });
-//      $(function(){
-//        $(document).click(function(event) {
-////          $(".hint-container__text").hide("fast");
-//            $(".hint-container__text").removeClass("on");
-//          event.stopPropagation();
-//        });
-//      });
-    
-//    $("#button").click(function(){
-//        $("#mydiv").show();
-//        $(document.body).click(function() {
-//            if ($(event.target).closest("#mydiv").length == 0)
-//                $("#mydiv").hide();     
-//        });
-//        return false;   
-//    });
-
+        $(document).click(function(event) {
+            $(".hint-container__text").removeClass("on");
+          event.stopPropagation();
+        });
 
     /* чиню поломанный дропдаун для выбора кода страны*/
     $("#btn-country").click(function(){
