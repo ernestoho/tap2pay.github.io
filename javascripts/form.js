8fb38160fccbe28bc696bff5b5aaf70632813400
Ledovit <drd8@mail.ru>
2016-10-31 12:52:13 +0300

    added script to set cursor in the end of string

--- javascripts/form.js
+++ javascripts/form.js
@@ -50,11 +50,33 @@
     }
   }
 	
-$('#ccnum').focus(function(){
-  var that = this;
-  setTimeout(function(){ that.selectionStart = that.selectionEnd = 10000; }, 0);
-});
+//$('#ccnum').focus(function(){
+//  var that = this;
+//  setTimeout(function(){ that.selectionStart = that.selectionEnd = 1000; }, 0);
+//});
+	
 	
 	
 })();
 
+function moveCaretToEnd(el) {
+    if (typeof el.selectionStart == "number") {
+        el.selectionStart = el.selectionEnd = el.value.length;
+    } else if (typeof el.createTextRange != "undefined") {
+        el.focus();
+        var range = el.createTextRange();
+        range.collapse(false);
+        range.select();
+    }
+}
+
+var textarea = document.getElementById("ccnum");
+
+textarea.onfocus = function() {
+    moveCaretToEnd(textarea);
+
+    // Work around Chrome's little problem
+    window.setTimeout(function() {
+        moveCaretToEnd(textarea);
+    }, 1);
+};
